ca1970a84c186405f6417d8443dfcd99d5d2db95
Mirns <mirnakabwe@gmail.com>
2022-03-24 21:11:16 +0000

    small challenges- odd, even, max, min, rounding off, abs nums

--- 04-js-types-numbers-exercise/index.js
+++ 04-js-types-numbers-exercise/index.js
@@ -6,6 +6,18 @@
  * anything else
  */
 
+function isOdd(num){
+
+    if(num%2 !== 0){
+        return "Is odd"
+    }else{
+        return "Not odd"
+    }
+
+
+}
+
+
 /**
  * Exercise 2
  *
@@ -14,6 +26,15 @@
  * anything else
  */
 
+function isEven(num){
+    if(num%2===0){
+        return "Is even"
+    } else{
+        return "Not even"
+    }
+}
+
+
 /**
  * Exercise 3
  *
@@ -21,12 +42,34 @@
  * and return a string "Is odd" if it's odd or "Is even" if it's even
  */
 
+function oddOrEven(num){
+    if(num%2===0)
+    {
+        return "Is even"
+    }
+    else if(num%2!==0)
+    {
+        return "Is odd"
+    }
+
+}
+
+
+
+
 /**
  * Exercise 4
  *
  * create function "roundedNumber" which takes a fraction as a parameter
  * and return rounded integer
  */
+//check
+function roundedNumber(floatNum){
+    return Math.round(floatNum)
+}
+
+
+
 
 /**
  * Exercise 5
@@ -35,12 +78,23 @@
  * and returns the largest integer less than or equal to a given number
  */
 
+function roundToLowerInteger(num){
+    return Math.floor(num)
+}
+
+
 /**
  * Exercise 6
  *
  * create function "roundToLargerInteger" which takes a fraction as a parameter
  * and returns rounded number up to the next largest integer
  */
+
+function roundToLargerInteger(num){
+    return Math.ceil(num)
+}
+
+
 
 /**
  * Exercise 7
@@ -49,6 +103,13 @@
  * and returns same number but positive
  */
 
+
+function convertToPositive(negNum){
+    return Math.abs(negNum)
+}
+
+
+
 /**
  * Exercise 8
  *
@@ -56,9 +117,22 @@
  * and returns the maximum number
  */
 
+
+function findMaxNumber(num1,num2,num3,num4){
+    let maxNum = Math.max(num1,num2,num3,num4)
+    return maxNum
+}
+
+
+
 /**
  * Exercise 9
  *
  * create function "findMinNumber" which takes a few numbers(at least 4 numbers) as a parameters
  * and returns the minimum number
  */
+
+function findMinNumber(num1,num2,num3,num4){
+    let minNum = Math.min(num1,num2,num3,num4)
+   return minNum
+}
